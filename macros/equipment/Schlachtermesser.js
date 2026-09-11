cc6bbc124a75cc21c6b4ab8e8e07e906733de1d6
Wero999 <felix.kittler@web.de>
2026-09-11 15:04:31 +0200

    Create Schlachtermesser.js

---
+++ macros/equipment/Schlachtermesser.js
@@ -0,0 +1,4 @@
+//Nachteil
+
+const isLeather = actor?.items.some(i => i.type === 'armor' && i.system.worn?.value && i.system.subcategory === 3);
+if(options.armor >= 3 && !isLeather) options.damage = Math.max(0, options.damage - 2);
